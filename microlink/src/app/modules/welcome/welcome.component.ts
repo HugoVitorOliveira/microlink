19443a5905db44d64576bb0067bb24cfbdc9af58
Hugo VO Moura <zoeiratheforever@gmail.com>
2025-03-03 22:11:54 -0300

    Create welcome component

--- microlink/src/app/modules/welcome/welcome.component.ts
+++ microlink/src/app/modules/welcome/welcome.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
+  standalone: true,
   selector: 'app-welcome',
-  imports: [],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule],
   templateUrl: './welcome.component.html',
-  styleUrl: './welcome.component.scss'
+  styleUrl: './welcome.component.scss',
 })
-export class WelcomeComponent {
-
-}
+export class WelcomeComponent {}
