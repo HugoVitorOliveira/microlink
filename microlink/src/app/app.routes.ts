2c209cf0fc409b4043e965e1ef23f3c3a07ddbda
Hugo VO Moura <zoeiratheforever@gmail.com>
2025-03-03 22:17:37 -0300

    Project config and routes

--- microlink/src/app/app.routes.ts
+++ microlink/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+  {
+    path: 'welcome',
+    loadChildren: () =>
+      import('@welcome/welcome.routes').then((r) => r.WELCOME_ROUTES),
+  },
+  { path: '**', redirectTo: 'app' },
+];
